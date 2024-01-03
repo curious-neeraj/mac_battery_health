bcd70d78a5865c07c2453937e2328707077cdb9b
Neeraj Jain <145219538+curious-neeraj@users.noreply.github.com>
2024-01-03 13:39:44 +0530

    Update Assistant Voice. Use Veena's Voice.

--- battery_health.sh
+++ battery_health.sh
@@ -7,23 +7,23 @@ then
 	newSource=$(pmset -g batt | grep -e "\w*\sPower" -o | cut -d " " -f1)
 	while [ $newSource != 'Battery' ]
 	do
-	say "Battery above $(( $charge-1 )) percent, please turn off charging." 
+	say "Battery above $(( $charge-1 )) percent, please turn off charging." -v Veena
 	sleep 5
 	newSource=$(pmset -g batt | grep -e "\w*\sPower" -o | cut -d " " -f1)
 	done
 	source='Battery'
-	say "Switched to battery power now." 
-	say "Thankyou Neeraj for your time!" 
+	say "Switched to battery power now." -v Veena
+	say "Thankyou Neeraj for your time!" -v Veena
 elif [[ $source='Battery' && $charge -lt 25 ]]
 then
 	newSource=$(pmset -g batt | grep -e "\w*\sPower" -o | cut -d " " -f1)
 	while [ $newSource != 'AC' ]
 	do
-	say "Battery below $(( $charge+1 )) percent, please turn on charging." 
+	say "Battery below $(( $charge+1 )) percent, please turn on charging." -v Veena
 	sleep 5
 	newSource=$(pmset -g batt | grep -e "\w*\sPower" -o | cut -d " " -f1)
 	done
 	source='AC'
-	say "Charging battery now." 
-	say "Thankyou Neeraj for your time!" 
+	say "Charging battery now." -v Veena
+	say "Thankyou Neeraj for your time!" -v Veena
 fi
